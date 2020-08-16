80c31eae3bd2a46f07dd900eb8beb3a170ffc016
Fouad Serradj <serradj.mohamed_fouad@courrier.uqam.ca>
2020-08-16 12:44:37 -0400

    Backdrop: Component to manage closing small screen navigation

---
+++ client/src/shared/components/UIElements/Backdrop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import './Backdrop.css';
+
+
+const Backdrop = props => {
+    const content = <div className="backdrop" onClick={props.onClick}></div>
+    return ReactDom.createPortal(content, document.getElementById('backdrop-hook'));
+}
+
+export default Backdrop;
