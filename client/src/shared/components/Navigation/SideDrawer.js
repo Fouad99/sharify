c2766fb057987741661503a3ab94d3fb2a46fe35
Fouad Serradj <serradj.mohamed_fouad@courrier.uqam.ca>
2020-08-16 14:09:07 -0400

    Refactoring

--- client/src/shared/components/Navigation/SideDrawer.js
+++ client/src/shared/components/Navigation/SideDrawer.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
+
 import './SideDrawer.css';
 
 export const SideDrawer = props => {
-    const content = <aside className="side-drawer">{props.children}</aside>;
+    const content = (
+        <CSSTransition
+            in={props.show}
+            timeout={300}
+            classNames="slide-in-left"
+            mountOnEnter
+            unmountOnExit
+        >
+            <div>
+                <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside>;
+            </div>
+        </CSSTransition>);
 
     return ReactDom.createPortal(content, document.getElementById('drawer-hook'));
 }
