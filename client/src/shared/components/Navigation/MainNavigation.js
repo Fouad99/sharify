254d0aa8e852927d43c8fb39e1b9b7e598bacc1e
Fouad Serradj <serradj.mohamed_fouad@courrier.uqam.ca>
2020-08-16 11:55:42 -0400

    MainNavigation: Add Navigation links components

--- client/src/shared/components/Navigation/MainNavigation.js
+++ client/src/shared/components/Navigation/MainNavigation.js
@@ -3,6 +3,8 @@ import './MainNavigation.css';
 import MainHeader from './MainHeader';
 import { Link } from 'react-router-dom';
 
+import NavLinks from './NavLinks';
+
 
 const MainNavigation = props => {
     return (
@@ -14,7 +16,7 @@ const MainNavigation = props => {
             </button>
             <h1 className="main-navigation__title"><Link to="/">APP NAME</Link></h1>
             <nav>
-                ...
+                <NavLinks />
             </nav>
         </MainHeader>
     )
