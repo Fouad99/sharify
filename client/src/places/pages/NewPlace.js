eb0b5ea2e9d42aa24c5fa1f7f876b77dcd6aa9a9
Fouad Serradj <serradj.mohamed_fouad@courrier.uqam.ca>
2020-09-10 13:40:32 -0400

    implement validators entry

--- client/src/places/pages/NewPlace.js
+++ client/src/places/pages/NewPlace.js
@@ -7,7 +7,7 @@ import Input from '../../shared/components/FormElements/Input/Input';
 const NewPlace = () => {
   return <div className="center">
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input element="input" type="text" label="Title" validators={[]} errorText="Please enter a valid title" />
     </form>
   </div>
 };
