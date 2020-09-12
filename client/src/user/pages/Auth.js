e8d58df6917229d9932207d033e8e5e691bc8097
Fouad Serradj <serradj.mohamed_fouad@courrier.uqam.ca>
2020-09-11 23:22:54 -0400

    Add Authentication page for user

---
+++ client/src/user/pages/Auth.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Card from '../../shared/components/UIElements/Card';
+import Input from '../../shared/components/FormElements/Input/Input';
+import Button from '../../shared/components/FormElements/Button/Button';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
+
+
+import './Auth.css';
+
+const Auth = () => {
+    const [formState, inputHandler] = useForm({
+        email: {
+            value: '',
+            isValid: false,
+        },
+        password: {
+            value: '',
+            isValid: false
+        }
+    }, false);
+
+    const loginSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState.inputs);
+    }
+
+    return (
+        <Card className="authentication">
+            <h2>Login required</h2>
+            <hr />
+            <form onSubmit={loginSubmitHandler}>
+                <Input
+                    element="input"
+                    id="email"
+                    type="email"
+                    label="Email"
+                    validators={[VALIDATOR_EMAIL()]}
+                    errorText="Please enter a valid Email address"
+                    onInput={inputHandler}
+                />
+                <Input
+                    element="input"
+                    id="password"
+                    type="password"
+                    label="password"
+                    validators={[VALIDATOR_MINLENGTH(8)]}
+                    errorText="Please enter a valid Email address"
+                    onInput={inputHandler}
+                />
+                <Button type="submit" disabled={!formState.isValid}>Login</Button>
+            </form>
+        </Card>
+    )
+};
+
+export default Auth;
