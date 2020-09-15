e9261fa13b5f8248f76dcbfcbd6d190c384262b6
Fouad Serradj <serradj.mohamed_fouad@courrier.uqam.ca>
2020-09-15 12:03:57 -0400

    add depend. developement

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Setting up parser
+app.use(bodyParser.urlencoded({extended: false});
+
+app.listen(5000, () => {
+	console.log('Connected server');	
+});
