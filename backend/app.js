7baad10bf7cd4e9e3c33992372f651592d145195
Fouad Serradj <serradj.mohamed_fouad@courrier.uqam.ca>
2020-09-15 12:06:17 -0400

    commits on depend.

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // Setting up parser
-app.use(bodyParser.urlencoded({extended: false});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.listen(5000, () => {
 	console.log('Connected server');	
