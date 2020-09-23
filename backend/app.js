3a6711b8b4003880bcc864a2bca93f487d3311e4
Fouad Serradj <serradj.mohamed_fouad@courrier.uqam.ca>
2020-09-23 17:32:23 -0400

    Handle wrong api route

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
 
 
 
@@ -13,6 +14,12 @@ app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
+
+app.use((req, res, next) => {
+	const error = new HttpError('Could not find proper route', 404);
+	next(error);
+});
+
 
 app.use((error, req, res, next) => {
 	if (res.headerSent) {
