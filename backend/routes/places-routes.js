5e118292db6c478f997895bb5b60af4bf7fc4ed4
Fouad Serradj <serradj.mohamed_fouad@courrier.uqam.ca>
2020-09-23 14:55:57 -0400

    Add routing for places

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -16,11 +16,33 @@ const DUMMY_DATA = [
     }
 ];
 
-router.get('/', (req, res, next) => {
-    console.log('GET request in Places');
-    res.status(200).json({
-        message: 'It works'
+router.get('/user/:uid', (req, res, next) => {
+    console.log('Get request in places/user');
+    const uid = req.params.uid;
+    const place = DUMMY_DATA.find(p => {
+        return p.creator === uid;
     });
+    if (!place) {
+        return res.status(404).json({ message: 'Could not find a plce with the provided user ID.' });
+    }
+
+    res.status(200).json({ place });
+})
+
+router.get('/:pid', (req, res, next) => {
+    console.log('GET request in Places');
+    const pid = req.params.pid;
+    const place = DUMMY_DATA.find(p => {
+        return p.id === pid;
+    });
+
+    if (!place) {
+
+        return res.status(404).json({ message: 'Could not find a place with the provided place ID.' });
+    }
+    res.status(200).json({ place });
 });
+
+
 
 module.exports = router;
